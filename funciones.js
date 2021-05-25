2108d2a0543d7a153bdad4cac5c5ab7f59a9c033
jaime <jaimemartinezmosquera@gmail.com>
2021-05-25 13:04:43 +0200

    iva fun

--- funciones.js
+++ funciones.js
@@ -14,8 +14,30 @@ let resultadoSuma=suma(3,4)
 console.log(resultadoSuma)  
 
 function calPrecioIva(precio,iva_pct){
-    const precio_iva=precio*(1+iva_pct/100).toFixed(2);
+    const precio_iva=(precio*(1+iva_pct/100)).toFixed(2);
     return precio_iva;
 }
+
 let pvp=calPrecioIva(10,21)
 console.log(pvp)
+
+let lista_pvp={
+    art1:10,
+    art2:4.23,
+    art3:24,
+    art4:7.5,
+    art5:24.8
+}
+
+const iva_recargado=21;
+
+for (const property in lista_pvp) {
+
+    console.log("Sin Iva: ");
+    console.log(`${property} = ${lista_pvp[property]}`);
+
+    console.log("Con Iva "+iva_recargado+"% : ");
+    console.log(`${property} = ${calPrecioIva(lista_pvp[property],iva_recargado)}`);
+
+  }
+
