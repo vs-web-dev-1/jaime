e3c73e6f345a660cf303ac2bde53f35271b846e0
jaime <jaimemartinezmosquera@gmail.com>
2021-05-26 12:05:46 +0200

    ej lista compra iva

--- iva_prod_conds.js
+++ iva_prod_conds.js
@@ -3,8 +3,9 @@ let iva_aplicable=21;
 
 let prod1={art_n:"zapatos",art_precio:"45",art_iva:true};
 let prod2={art_n:"pan",art_precio:"2",art_iva:false};
+let prod3={art_n:"Chaqueta",art_precio:"35",art_iva:true};
 
-lista_compra=[prod1,prod2];
+lista_compra=[prod1,prod2,prod3];
 
 for(let i=0;i < lista_compra.length; i++) {
     
@@ -15,8 +16,10 @@ for(let i=0;i < lista_compra.length; i++) {
         precio_final=lista_compra[i].art_precio;
     }
 
-
+    console.log("-----------------------------------");
     console.log("Artículo "+i+": "+lista_compra[i].art_n+
     " con precio base= "+lista_compra[i].art_precio+". "+
     "Iva: "+lista_compra[i].art_iva+". Precio fin: "+precio_final);
+    console.log("-----------------------------------");
+
   }
