b0d0c40be7f57e91dc398dc7c7de9e8bf5a4f26f
jaime <jaimemartinezmosquera@gmail.com>
2021-05-26 09:45:18 +0200

    if

---
+++ control_f.js
@@ -0,0 +1,32 @@
+
+let a=1;
+let b=4;
+console.log("------------")
+console.log("------------")
+console.log("a es "+a)
+console.log("b es "+b)
+console.log("------------")
+console.log("------------")
+console.log("control menor o igual que:")
+
+if(a<=b){
+    console.log("a es menor que b")
+}else{
+    console.log("a no es menor que b")
+}
+console.log("------------")
+console.log("control mayor o igual que:")
+
+if(a>=b){
+    console.log("a es mayor o igual que b")
+}else{
+    console.log("a no es mayor o igual que b")
+}
+console.log("------------")
+console.log("control menor o igual que (apuntes):")
+
+if(a=>b){
+    console.log("a es igual o mayor que b")
+}else{
+    console.log("a no es igual o mayor que b")
+}
