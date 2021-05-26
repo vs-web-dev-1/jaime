c27ebcb19c5e4be7e0920660693cf7c19c8bebfc
jaime <jaimemartinezmosquera@gmail.com>
2021-05-26 10:23:20 +0200

    control flujo

--- control_f.js
+++ control_f.js
@@ -3,11 +3,11 @@ let a=1;
 let b=4;
 console.log("------------")
 console.log("------------")
-console.log("a es "+a)
-console.log("b es "+b)
+console.log("a es "+a);
+console.log("b es "+b);
 console.log("------------")
 console.log("------------")
-console.log("control menor o igual que:")
+console.log("control menor o igual que:");
 
 if(a<=b){
     console.log("a es menor que b")
@@ -29,4 +29,24 @@ if(a=>b){
     console.log("a es igual o mayor que b")
 }else{
     console.log("a no es igual o mayor que b")
+}
+console.log("------------")
+console.log("------------")
+console.log("------------")
+
+let animales=["orca","gato","mono","canario"];
+animales.push("vaca");
+console.log("Lista de animales: "+animales);
+console.log("------------")
+console.log("------------")
+
+let i = 0;
+while (i <= 10) {
+    if(i%2===0){
+        animales.push(i)
+    }
+
+ console.log("Lista actual: "+animales);
+ console.log("------------")
+ i++;
 }
